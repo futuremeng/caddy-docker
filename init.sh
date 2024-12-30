080cfefa8fa8bfe99127aa33b1cd924425af6c2f
be_loving@163.com <>
2024-12-30 12:22:25 +0800

    rename

--- init.sh
+++ init.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 IPREX4='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
-cd /data || exit
+cd /config || exit
 if [ -z "$DNS" ]; then
     echo "DNS not set, use default DNS."
 else
     echo "Apply DNS: ""$DNS"
     echo "$DNS" | sed 's/"//g' | grep -Eo "[^,]+" | grep -Eo "$IPREX4" | sed "s/^/nameserver /g" >/etc/resolv.conf
 fi
-ls -shan /data
-sed 's/\r$//' /data/caddyfile.txt >/tmp/caddyfile
-exec caddy run --config /tmp/caddyfile --adapter caddyfile
+ls -shan /config
+exec caddy run --config /config/caddyfile --adapter caddyfile
